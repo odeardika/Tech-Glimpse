795c504853da3cfea0c905048679d2cc1273577a
Ode Ardika <104220059+odeardika@users.noreply.github.com>
2025-04-21 19:55:23 +0800

    feat: make the main page responsive

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,19 @@ import NewsCard from "@/components/NewsCard/NewsCard";
 
 export default function Home() {
   return (
-    <div>
-      <header className="flex flex-col items-start gap-2 justify-between py-8 px-48">
+    <div className="px-8 md:px-24">
+      <header className="flex flex-col items-start gap-2 justify-between py-8 ">
         <h1 className="text-4xl font-semibold">Tech Glimpse</h1>
         <h2 className="text-xl text-slate-600">Stay updated with the latest technology news every day</h2>
       </header>
 
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 px-48">
+      <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-5 ">
+        <NewsCard />
+        <NewsCard />
+        <NewsCard />
+        <NewsCard />
+        <NewsCard />
+        <NewsCard />
         <NewsCard />
         <NewsCard />
         <NewsCard />
