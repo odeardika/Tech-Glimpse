82aca1aee478bc03596ba5cb2778ef92a8df1d4f
Ode Ardika <104220059+odeardika@users.noreply.github.com>
2025-04-21 20:43:59 +0800

    feat: adding Popup Menu to subcribe news latter

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
-import Image from "next/image";
 import NewsCard from "@/components/NewsCard/NewsCard";
+import PopupMenu from "@/components/PopupMenu/PopupMenu";
 
 export default function Home() {
   return (
     <div className="px-8 md:px-24">
-      <header className="flex flex-col items-start gap-2 justify-between py-8 ">
-        <h1 className="text-4xl font-semibold">Tech Glimpse</h1>
-        <h2 className="text-xl text-slate-600">Stay updated with the latest technology news every day</h2>
+      <header className="flex justify-between py-8 items-start md:items-center ">
+        <div className="flex flex-col items-start gap-2">
+          <h1 className="text-2xl md:text-4xl font-semibold">Tech Glimpse</h1>
+          <h2 className="text-base md:text-xl text-slate-600">Stay updated with the latest technology news every day</h2>
+        </div>
+        <div className="pt-2 md:pt-0">
+          <PopupMenu />
+        </div>
+
       </header>
 
       <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-5 ">
