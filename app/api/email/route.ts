c89acdfaa1d9f37cf1b16130bfe19d62d4398f93
Ode Ardika <dirfanardika@gmail.com>
2025-06-24 11:54:53 +0800

    fix: change the email endpoint into GET

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import { getUserEmail } from "@/modules/supabase/server";
 import News from "@/types/News";
 
-export async function POST() {
+export async function GET() {
     // get artcle
     const article : News[] = (await axios.get(process.env.NEXT_PUBLIC_SERVER_ENDPOINT+"news")).data;
     const fillteredArticle = article.filter(article => (article.description && (article.description.length >= 20)) && (article.image))
