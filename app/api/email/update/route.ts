86e1ff099caafee669081fba4fbeeb61adaf4347
Ode Ardika <dirfanardika@gmail.com>
2025-07-14 21:37:09 +0800

    fix: fix error when build

--- app/api/email/update/route.ts
+++ app/api/email/update/route.ts
@@ -1,9 +1,10 @@
-import { DeactivateUser, activeDatabases } from "@/modules/supabase/server";
+import { DeactivateUser, activeAddDatabases, activeDeleteDatabases } from "@/modules/supabase/server";
 
 export async function PUT () {
     // deactivate spesific user to keep database active
     const res = await DeactivateUser(15);
-    activeDatabases();
+    activeAddDatabases();
+    activeDeleteDatabases();
 
     return Response.json(res);
 }
