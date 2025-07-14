25ba6f388bcd0931b0565a1376b55169e471d779
Ode Ardika <dirfanardika@gmail.com>
2025-07-14 08:04:15 +0800

    fix : update the supabase activetor

--- app/api/email/update/route.ts
+++ app/api/email/update/route.ts
@@ -1,8 +1,9 @@
-import { DeactivateUser } from "@/modules/supabase/server";
+import { DeactivateUser, activeDatabases } from "@/modules/supabase/server";
 
 export async function PUT () {
     // deactivate spesific user to keep database active
     const res = await DeactivateUser(15);
+    activeDatabases();
 
     return Response.json(res);
 }
