ae868b2212402071704bd23b104b736f9fdf4da8
Ode Ardika <dirfanardika@gmail.com>
2025-08-01 18:54:55 +0800

    feat: create endpoint to get news

--- app/api/news/route.ts
+++ app/api/news/route.ts
@@ -1,6 +1,12 @@
+import { getNews } from "@/modules/news/news"
+import News from "@/types/News"
+
 export async function GET() {
+    const news : News[] = await getNews();
+
     return Response.json({
         status : 200,
-        message : "success"
+        message : "success",
+        news : news
     })
 }
