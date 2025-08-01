d10ae5e8109c4df2017ece78277006a0b1c8ba0a
Ode Ardika <dirfanardika@gmail.com>
2025-08-01 19:09:26 +0800

    feat: connect the client with the new endpoint

--- app/api/news/route.ts
+++ app/api/news/route.ts
@@ -1,12 +1,18 @@
-import { getNews } from "@/modules/news/news"
-import News from "@/types/News"
+import { getNews } from "@/modules/news/news";
+import News from "@/types/News";
 
 export async function GET() {
-    const news : News[] = await getNews();
+  const news: News[] = await getNews();
 
+  if (news.length > 0) {
     return Response.json({
-        status : 200,
-        message : "success",
-        news : news
-    })
+      status: 200,
+      message: "success",
+      news: news,
+    });
+  }
+  return Response.json({
+    status : 400,
+    message : "failed to fetch news"
+  })
 }
